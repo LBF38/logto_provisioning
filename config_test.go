01542df85c2f93812032ff5146505640cf516b09
LBF38 <contact.lbf38@gmail.com>
2024-09-29 14:45:41 +0200

    refactor(config): :white_check_mark: update tests to table based tests
    for easily adding new test cases

--- config_test.go
+++ config_test.go
@@ -6,48 +6,33 @@ import (
 )
 
 func TestNewConfig(t *testing.T) {
-	t.Run("Logto config", func(t *testing.T) {
-		got, err := NewConfig("testdata/config_logto.yaml")
-		want := LogtoConfig{
-			url:        "https://[tenant-id].logto.app/api",
-			app_id:     "APP_ID",
-			app_secret: "APP_SECRET",
-		}
+	tests := []struct {
+		name string
+		file string
+		want LogtoConfig
+	}{
+		{
+			name: "Logto config",
+			file: "testdata/config_logto.yaml",
+			want: LogtoConfig{
+				url:        "https://[tenant-id].logto.app/api",
+				app_id:     "APP_ID",
+				app_secret: "APP_SECRET",
+			},
+		},
+	}
 
-		if err != nil {
-			t.Errorf("Error should be nil, but got %v", err)
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewConfig(tt.file)
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v, want %v", got, want)
-		}
-	})
+			if err != nil {
+				t.Errorf("Error should be nil, but got %v", err)
+			}
 
-	// got, err := NewConfig("testdata/config_minimal.yaml")
-	// want := LogtoConfig{
-	// 	url:        "https://[tenant-id].logto.app/api",
-	// 	app_id:     "APP_ID",
-	// 	app_secret: "APP_SECRET",
-	// 	resources: []LogtoResource{
-	// 		{
-	// 			name:        "https://api.store.io/orders",
-	// 			permissions: []string{"read:order", "write:order", "delete:order"},
-	// 		},
-	// 	},
-	// 	roles: []LogtoRole{
-	// 		{
-	// 			name:        "order_admin",
-	// 			permissions: []string{"read:order", "write:order", "delete:order"},
-	// 		},
-	// 	},
-	// }
-
-	// if err != nil {
-	// 	t.Errorf("Error should be nil, but got %v", err)
-	// }
-
-	// if !reflect.DeepEqual(got, want) {
-	// 	t.Errorf("got %v, want %v", got, want)
-	// }
-
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
