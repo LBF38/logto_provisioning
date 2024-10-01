1a603be44fe0ebc42f3776bbff9e8062b2c47b7b
LBF38 <contact.lbf38@gmail.com>
2024-10-01 19:25:44 +0200

    docs(config): :bulb: add some comments/docs in source code

--- config.go
+++ config.go
@@ -6,23 +6,30 @@ import (
 	"github.com/knadh/koanf/providers/file"
 )
 
+// Represents the struct extracted from the yaml config file
 type Config struct {
 	Logto     LogtoConfig
 	Resources []LogtoResource
 	Roles     []LogtoRole
 }
 
+// The basic logto config for communicating w/ the Logto instance.
 type LogtoConfig struct {
 	Url       string
 	AppID     string
 	AppSecret string
 }
 
+// Represents a logto resource
+//
+// This is for defining RBAC on each resource
 type LogtoResource struct {
 	BaseUrl   string
 	Endpoints map[string][]string
 }
 
+// Logto roles
+// See testdata folder for config examples
 type LogtoRole map[string]map[string][]string
 
 func NewConfig(filename string) (Config, error) {
