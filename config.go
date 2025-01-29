02ac8b53519ef094de1a27943af6e2d38ceb02f3
LBF38 <contact.lbf38@gmail.com>
2025-01-29 11:39:51 +0100

    feat(project): :sparkles: add func to fetch access token
    useful for interacting w/ Management API later and do provisioning

--- config.go
+++ config.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+
 	"github.com/knadh/koanf"
 	"github.com/knadh/koanf/parsers/yaml"
 	"github.com/knadh/koanf/providers/file"
@@ -43,4 +51,57 @@ func NewConfig(filename string) (Config, error) {
 	// log.Println("config unmarshal: ", config)
 
 	return config, nil
+}
+
+type AccessTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int    `json:"expires_in"`
+	TokenType   string `json:"token_type"`
+	Scope       string `json:"scope"`
+}
+
+func (c Config) FetchAccessTokenResponse() (AccessTokenResponse, error) {
+	bodyParams := map[string]string{
+		"grant_type": "client_credentials",
+		"resource":   "https://default.logto.app/api",
+		"scope":      "all",
+	}
+
+	formData := url.Values{}
+	for key, value := range bodyParams {
+		formData.Set(key, value)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, c.Logto.Url+"/oidc/token", bytes.NewBufferString(formData.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth(c.Logto.AppID, c.Logto.AppSecret)
+
+	if err != nil {
+		return AccessTokenResponse{}, fmt.Errorf("HTTP Request error: %v", err)
+	}
+
+	log.Println("HTTP request: ", req)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return AccessTokenResponse{}, fmt.Errorf("HTTP Response error: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return AccessTokenResponse{}, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return AccessTokenResponse{}, fmt.Errorf("error reading response body: %v", err)
+	}
+
+	var result AccessTokenResponse
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return AccessTokenResponse{}, fmt.Errorf("error unmarshalling response body: %v", err)
+	}
+
+	return result, nil
 }
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
 	fmt.Println("Logto Provisioning")
+
+	config, err := NewConfig("provisioning/logto_provisioning_dev.yaml")
+
+	if err != nil {
+		log.Fatalf("Failed loading config file: %v\n", err)
+	}
+
+	// Fetch access token
+	accessTokenResponse, err := config.FetchAccessTokenResponse()
+	if err != nil {
+		log.Fatalf("Error getting access token: %v", err)
+	}
+
+	// Interact w/ Management API (provisioning, fetching data, ...)
+	log.Printf("access_token: %v\n", accessTokenResponse.AccessToken)
+
 }
