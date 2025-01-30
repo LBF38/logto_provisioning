f86d44d6b539bf9175af1eb8654f474f7e50d2a7
LBF38 <contact.lbf38@gmail.com>
2025-01-30 13:41:35 +0100

    feat(project): :sparkles: add new funcs for provisioning
    update openapi spec source file
    update provisioning file w/ creds
    add new funcs for provisioning
    remove unuseful file

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 
 	openapi "github.com/lbf38/logto_provisioning/logtoClient/client"
 	"golang.org/x/oauth2"
+)
+
+// Create OpenAPI instance
+var (
+	cfg    = openapi.NewConfiguration()
+	client = openapi.NewAPIClient(cfg)
 )
 
 func main() {
@@ -27,9 +32,6 @@ func main() {
 	}
 	log.Printf("Access Token response: %v\n", accessTokenResponse)
 
-	// Create OpenAPI instance
-	cfg := openapi.NewConfiguration()
-	client := openapi.NewAPIClient(cfg)
 	fmt.Printf("OpenAPI configuration: %+v\n", *cfg)
 	fmt.Printf("OpenAPI client: %+v\n", *client)
 
@@ -42,25 +44,76 @@ func main() {
 
 	//! Provision Logto instance
 	// Resources provisioning
-	req := *openapi.NewCreateResourceRequest("test openapi", "http://test.io")
-	resp, r, err := client.ResourcesAPI.CreateResource(auth).CreateResourceRequest(req).Execute()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error when calling `ResourcesAPI.CreateResource``: %v\n", err)
-		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	for _, resource := range config.Resources {
+		for endpoint, scopes := range resource.Endpoints {
+			createdResource, err := createResource(auth, endpoint, resource.BaseUrl+endpoint)
+			if err != nil {
+				log.Fatalf("Resources provisioning: %v", err)
+			}
+			for _, scope := range scopes {
+				createdScope, err := createResourceScope(auth, scope, "description", createdResource)
+				if err != nil {
+					log.Fatalf("Resources provisioning: %v", err)
+				}
+				fmt.Println(createdScope)
+			}
+		}
 	}
-	// response from `CreateResource`: ListResources200ResponseInner
-	fmt.Fprintf(os.Stdout, "Response from `ResourcesAPI.CreateResource`: %v\n", resp)
-
-	resp2, r, err := client.ResourcesAPI.ListResources(auth).Execute()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error when calling `ResourcesAPI.ListResources``: %v\n", err)
-		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
-	}
-	fmt.Fprintf(os.Stdout, "Response from `ResourcesAPI.ListResources`: %v\n", resp2)
 
 	// Roles provisioning
+	for _, role := range config.Roles {
+		for role_name, resources := range role {
+			fmt.Println(resources)
+			// TODO: link each role w/ resources scopes
+			createdRole, err := createRole(auth, role_name, nil)
+			if err != nil {
+				log.Fatalf("Roles provisioning: %v", err)
+			}
+			fmt.Println(createdRole)
+		}
+	}
 
 	// Users provisioning
 
 	// Organizations provisioning
+}
+
+func createResource(ctx context.Context, name, indicator string) (*openapi.ListResources200ResponseInner, error) {
+	req := *openapi.NewCreateResourceRequest(name, indicator)
+	resp, r, err := client.ResourcesAPI.CreateResource(ctx).CreateResourceRequest(req).Execute()
+	if err != nil {
+		return nil, fmt.Errorf("error when calling `ResourcesAPI.CreateResource``: %v\nFull HTTP response: %v", err, r)
+	}
+	// response from `CreateResource`: ListResources200ResponseInner
+	return resp, nil
+}
+
+func createResourceScope(ctx context.Context, scope, description string, createdResource *openapi.ListResources200ResponseInner) (*openapi.ListResources200ResponseInnerScopesInner, error) {
+	req := openapi.NewCreateResourceScopeRequest(scope)
+	req.SetDescription(description)
+	resp, r, err := client.ResourcesAPI.CreateResourceScope(ctx, createdResource.Id).CreateResourceScopeRequest(*req).Execute()
+	if err != nil {
+		return nil, fmt.Errorf("error with Resources scope provisioning: %v\nFull HTTP response: %v", err, r)
+	}
+	return resp, nil
+}
+
+func createRole(ctx context.Context, role_name string, scopes []string) (*openapi.ListApplicationRoles200ResponseInner, error) {
+	req := openapi.NewCreateRoleRequest(role_name, "some description")
+	if scopes != nil {
+		req.SetScopeIds(scopes)
+	}
+	req.SetIsDefault(false)
+	log.Printf("createRole request: %+v\n", req)
+
+	resp, r, err := client.RolesAPI.CreateRole(ctx).CreateRoleRequest(*req).Execute()
+	if r.StatusCode < 300 {
+		return resp, nil
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("error with Roles provisioning: %v\nFull HTTP response: %v", err, r)
+	}
+
+	return resp, nil
 }
